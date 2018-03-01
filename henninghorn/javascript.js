ef5b011a315256cd70643d136135791a20821294
Henning Horn <hh@henninghorn.dk>
2018-03-01 16:19:14 +0100

    Solved the assignment

--- henninghorn/javascript.js
+++ henninghorn/javascript.js
@@ -1,2 +1,8 @@
 // Hello World!
 console.log('Hello World!')
+
+// Create a object constructor for a Drink object
+// The constructor needs 3 parameters:
+// name (String), price (Integer), ingredients (Array)
+
+/* This is my awesome solution */
