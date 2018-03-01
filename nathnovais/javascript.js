01777892c7982d8bce563aa80b6d6644e4a6fd12
Nathalia Novais <nathnovais@gmail.com>
2018-03-01 16:31:43 +0100

    drink solution

---
+++ nathnovais/javascript.js
@@ -0,0 +1,60 @@
+// Hello World!
+console.log('Hello World!')
+/*
+let a = 3
+function addition (a, b) {
+  console.log(a+b);
+}
+addition(3, 5)
+addition(a, 5)
+
+function getHelloGreeting(name) {
+  return 'Hello' + name
+  return `Hello ${name}`
+
+}
+let PersonObject = {
+  name: 'Hening Horn',
+  city: 'Aarhus',
+  favorites: {
+    cars: [
+      'Tesla',
+      'Volvo'
+    ],
+  artists: [
+    'Bon Iver',
+    'Keaton henson',
+    'Nephew'
+  ]
+  }
+}
+
+//accesing properties:
+PersonObject.name
+PersonObject.favorites.cars
+PersonObject.favorites.artists[1]
+
+console.log('PersonObject', PersonObject);
+
+
+//constructor function
+function person(name, city, age) {
+  this.name = name
+  this.city = city
+  this.age = age
+}
+
+//creating an instance of an object (use keyword new)
+let myPerson = new Person('Henning Horn', 'Aarhus', 28)
+console.log('myPerson', myPerson);
+*/
+
+//creating a constructor of a drink object – name(string) price(integer) ingredients(Array)
+function Drink(name, price, ingredients) {
+  this.name = name
+  this.price = price
+  this.ingredients = ingredients
+}
+
+let myDrink = new Drink('margarita', 85, ['tequila', 'lime', 'sugar'])
+console.log('My Drink:', myDrink);
