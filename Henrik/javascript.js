7b5d7f80d466f1d55cbb793bb76c96bdc35396bc
henrik.schytze <henrikschytze@gmail.com>
2018-03-01 16:08:57 +0100

    my solutions

--- Henrik/javascript.js
+++ Henrik/javascript.js
@@ -1,2 +1,47 @@
 // Hello World!
-console.log('Hello World!')
+/*
+function addition (a, b) {
+
+  result = a + b;
+  console.log(result);
+}
+
+let arrowGreeting = (name) => {
+  console.log("hello " + name);
+}
+
+arrowGreeting("Henrik");
+
+
+//function declarations are available within global scope
+
+//objects
+//object literal defines object right away
+//Below is a nested object as well
+/*let person1 = {
+  name: "Henrik",
+  city: "Aarhus",
+  favorites: {
+    books: ["Day one", "day two"]
+  }
+}
+console.log("test", person.favorites.books[1]);*/
+
+//Alternatively u can use constructor functions. If u want multiple objects of same kind for example
+//this references the object ure creating
+function person(name, city, age) {
+  this.name=name
+  this.city=city
+  this.age=age
+}
+let henrik = new person ("henrik", "Aarhus", 24)
+console.log("henrik", henrik)
+
+
+function drink (name, price, ingridients) {
+  this.name=name
+  this.price=price
+  this.ingridients=ingridients
+}
+let dryMartini = new drink("dry martini", "50kr", ["gin", "white vermouth", "garnish"]);
+console.log("testing", dryMartini)
