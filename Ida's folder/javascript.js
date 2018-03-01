27f067eff21636a429d76f455dc062d6c1488d50
Ida Staerkjaer <ida_sn@hotmail.com>
2018-03-01 16:19:19 +0100

    Cosmopolitan

---
+++ Ida's folder/javascript.js
@@ -0,0 +1,20 @@
+// Hello World!
+console.log('Hello World!')
+
+let DrinkObject {
+  name: 'Cosmopolitan',
+  price: '80',
+
+  let ingredients: {
+    alcohol: [
+      'vodka',
+      'cointreau'
+    ],
+    juice:[
+      'cranberry juice',
+      'lime juice'
+    ]
+  }
+}
+
+console.log(DrinkObject);
