f8733ae06f976ef41a213b707b8658f914d303f3
Lena Buhl <l-buhl@hotmail.com>
2018-03-01 16:54:23 +0100

    New exercise

--- lenabuhl/javascript.js
+++ lenabuhl/javascript.js
@@ -16,6 +16,23 @@ function Drink (name, price, ingredients) {
   this.ingredients = ingredients
 }
 
-let sunrise = new Drink ('Sunrise', 50, ['Tequila', 'Lemon'])
+let sunrise = new Drink ('Sunrise', 50, ['Tequila', 'Lemon'])  //correct
 
 console.log(sunrise)
+
+
+//New Exercise
+
+function Person (name, age) {
+  this.name = name
+  this.age = age
+
+  this.canIDriveInDK = function() {
+    age > 18
+
+  }
+
+}
+
+let john = new Person ('John', 20)
+console.log(john.canIDriveInDK)
