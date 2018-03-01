5f582c93c4fee14e1db2727fa50c92f6ea50f94b
Sonia Johansen <sonia.johansen@hotmail.com>
2018-03-01 16:53:15 +0100

    Summery

--- Sonia-Johansen/javascript.js
+++ Sonia-Johansen/javascript.js
@@ -10,8 +10,8 @@ addition(5, 5)
 /* how to do objects (let emptyObject ={ name = Sonia city = Aarhus })
 let PersonObject = { name: city:}
 PersonObject.name => Sonia
-This is called object litteral
-You can also do alot of proberties in the same object.
+This is called object litterel
+You can also do a lot of proberties in the same object.
 Eksample:
 */
 
@@ -23,3 +23,17 @@ this.engredients = engredients
 let sunrise = new Drink('Sunrise', 47, ['vodka', 'Juice'])
 
 console.log(sunrise)
+
+
+function Person(name, age) {
+  this.name = name
+  this.age = age
+
+  this.canIDriveInDenmark = function () {
+    let Boolean = true;
+    if Boolean = >18
+    console.log('You can drive in Denmark ' + this.name)
+  }
+}
+let sonia = new Person('Sonia', 24)
+sonia.canIDriveInDenmark()
