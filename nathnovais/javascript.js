c5e247e09a685469db559907eb81ffb95067a588
Nathalia Novais <nathnovais@gmail.com>
2018-03-01 17:42:06 +0100

    solution 3

--- nathnovais/javascript.js
+++ nathnovais/javascript.js
@@ -63,22 +63,55 @@ function Drink(name, price, ingredients) {
 let myDrink = new Drink('margarita', 85, ['tequila', 'lime', 'sugar'])
 console.log('My Drink:', myDrink);
 
+//Reuse drink object, add 5 different drinks in array
+//and use filter method to display drinks priced bellow 50
+
+
+let drinks = [
+  new Drink ('White russian', 45, ['milk, vodka']),
+  new Drink ('Cosmopolitan', 60, ['vodka', 'cranberry juice']),
+  new Drink ('long island ice tea', 70, ['vodka', 'coca cola', 'rum']),
+  new Drink ('Strawberry Daquiri', 50, ['vodka', 'strawberry juice'])
+]
+console.log('Drinks under 50kr', drinks.filter(Drink => Drink.price <= 50))
+
+
+
 //Create a person object constructor, 2 parameters: name(string), age(integer) -> real values are arguments
 //(argument is the value and parameters is what defines the function)
 //One method: canIDriveInDenmark() => check if age is abot or equal 18
 //return true or false
 
 
-function Person(name, age) {
+/* function Person(name, age) {
   this.name = name
   this.age = age
   this.canIDriveInDenmark = function() {
+    //return this.age >= 18
     if(age => 18) {
-      console.log('You are allowed to drive in Denmark')
+      //console.log('You are allowed to drive in Denmark')
+      return true
     }  else {
-        console.log('You are not allowed to drive in Denmark')
+        //console.log('You are not allowed to drive in Denmark')
+        return false
     }
 }
-}
 let myPerson = new Person('Nathalia', 31)
-console.log(Person.canIDriveInDenmark)
+console.log('Can I Drive?', myPerson.canIDriveInDenmark()); */
+
+/* parrot function
+function parrot(name) {
+  this.name = name
+
+  this.say = function(text = `Hello, my name is ${this.name}`) {
+    let msg = new SpeechSynthesisUtterance(text)
+    msg.lang = 'en-US'
+    //msg.voice = SpeechSynthesis.getVoice
+    window.SpeechSynthesis.speak(msg)
+  }
+}
+
+//another way of making constructors
+const Gift = (description, type, price) => {
+  return {description, type, price}
+} */
