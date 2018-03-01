a1f27a7dfcda98f1c799670f064c65cd6a13d5fb
Sonia Johansen <sonia.johansen@hotmail.com>
2018-03-01 16:21:20 +0100

    Summery

---
+++ Sonia-Johansen/javascript.js
@@ -0,0 +1,24 @@
+// Hello World!
+console.log('Hello World!')
+
+function addition (a, b) {
+  console.log (a + b)
+}
+
+adition(5, 5)
+
+/* how to do objects (let emptyObject ={ name = Sonia city = Aarhus })
+let PersonObject = { name: city:}
+PersonObject.name => Sonia
+This is called object litteral
+You can also do alot of proberties in the same object.
+Eksample:
+*/
+function Drink (name, price, engredients) {
+this.name = name
+this.price = price
+this.engredients = engredients
+}
+let sunrise = new Drink ("Sunrise", 47,) ["vodka"]
+
+console.log(sunrise)
