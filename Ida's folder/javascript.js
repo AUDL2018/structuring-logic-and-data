d995cdf78bb1840f266b5aed140465832162a9fb
Ida Staerkjaer <ida_sn@hotmail.com>
2018-03-01 17:00:17 +0100

    Driver?

--- Ida's folder/javascript.js
+++ Ida's folder/javascript.js
@@ -2,15 +2,13 @@
 console.log('Idas Branch!')
 
 function Driver(name, age){
-  this.name = name,
-  this.age = age,
+  this.name = name;
+  this.age = age;
   this.legal = function(){
-
-    console.log('Hello, my name is ' + this.name + 'I am ' + this.age + 'years old!')
-
-  }
+    return this.age >= 18
 }
 
 let charlie = new Driver('Charlie', '17')
 
-charlie.legal(age => 18)
+console.log('Hello, my name is ' + this.name + 'I am ' + this.age + 'years old!');
+console.log('Can you drive?', charlie.Driver());
